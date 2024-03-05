c9fc78fed1cac91ef7ea7684b8fc633201ef170f
kch0527 <jhgu128@naver.com>
2024-03-05 11:30:37 +0900

    프로젝트 생성

---
+++ src/main/java/com/side_project/health/HealthApplication.java
@@ -0,0 +1,14 @@
+package com.side_project.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApplication.class, args);
+		System.out.println("commit test");
+	}
+
+}
---
+++ src/test/java/com/side_project/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side_project.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
